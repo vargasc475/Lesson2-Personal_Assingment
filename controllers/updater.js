ee3756feb2cdf8e83f3c5b1865d8809e0093fbd1
Christian <vargasc475@gmail.com>
2023-06-13 21:16:39 -0400

    update controller

--- controllers/updater.js
+++ controllers/updater.js
@@ -1,13 +1,12 @@
 const mongodb = require('../db/connection');
 const ObjectId = require('mongodb').ObjectId;
 
-const findOneDocument = async (req, res, next) => {
+const findOneDocument = async (req, res) => {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.databaseConnected().db("cse341").collection("Contacts").find({ _id: userId });
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists[0]);
-        console.log(lists);
     });
   
 }
