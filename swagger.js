001e609c0260796ac7b52d5aec050bcae58a0ff4
Christian <vargasc475@gmail.com>
2023-06-15 22:03:22 -0400

    Fixed swagger link

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'localhost:8080/contacts',
+  host: 'lesson2-personal-assingment.onrender.com',
   schemes: ['https'],
 };
 
